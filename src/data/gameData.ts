091969d7ff54613f9b2693e8cf71deac40132136
Ionut Cojocaru <111282912+IonutInit@users.noreply.github.com>
2023-04-06 16:00:11 +0100

    created simple event handler

--- src/data/gameData.ts
+++ src/data/gameData.ts
@@ -1,6 +1,6 @@
 const gameData = {
   gameState: 3,
-  chapter: 5,
+  chapter: 64,
   alice: {
     agility: 6,
     logic: 6,
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import Console from "../game/Console";
 import ChapterContent from "../game/ChapterContent";
 import SimpleOption from "../options/SimpleOption";
@@ -12,6 +14,32 @@ const content: Content = importedContent;
 
 function Game({ dispatch, state }: Props) {
   const chapter = content[state.chapter];
+
+  const handleEvent = () => {
+    if (chapter.event !== undefined) {
+      const attribute = chapter.event[0].name;
+      const amount = chapter.event[0].change;
+      const eventIsGood = amount > 0;
+      const multiplePoints = Math.abs(amount) !== 1;
+      dispatch({
+        type: "modify_attribute",
+        payload: {
+          attribute,
+          amount,
+        },
+      });
+      dispatch({
+        type: "update_log",
+        payload: `You have ${eventIsGood ? "won" : "lost"} ${Math.abs(
+          amount
+        )} ${attribute} point${multiplePoints ? "s" : ""}!`,
+      });
+    }
+  };
+
+  useEffect(() => {
+    handleEvent();
+  }, [chapter, dispatch, state.chapter]);
 
   const caseForEndOfSample = chapter.sample_end;
   const caseForTest = chapter.test;
